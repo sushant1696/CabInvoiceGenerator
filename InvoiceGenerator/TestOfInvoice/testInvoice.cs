c301d4d91f100857d2ad1b43ae53c4270edb02d2
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-01 15:48:58 +0530

    [sushant]Refactore .step 1 pass value according to TDD approach

--- InvoiceGenerator/TestOfInvoice/testInvoice.cs
+++ InvoiceGenerator/TestOfInvoice/testInvoice.cs
@@ -21,23 +21,6 @@ namespace TestOfInvoice
             int expect = 32;
             Assert.AreEqual(expect, result);
         }
-        /// <summary>
-        /// Step 2 GivenMultiRide_ShouldReturnTotalFare
-        /// </summary>
-        [Test]
-        public void GivenMultiRide_ShouldReturnTotalFare()
-        {
-            //It creates the List for multi ride .
-            List<Ride> list = new List<Ride> {
-                new Ride { distance_in_km = 12, time_in_min = 0 },
-                new Ride { distance_in_km = 0, time_in_min = 55 },
-                new Ride { distance_in_km = 0, time_in_min = 10 },
-                 new Ride { distance_in_km = 0, time_in_min = 15 }
-            };
-            InvoiceGenerators obj = new InvoiceGenerators();
-            var actual =obj.CalculateMultiRideFare(list);
-            double expect = 200;//expected result 12*10+55+10+15=200
-            Assert.AreEqual(expect, actual);
-        }
+        
     }
 }
