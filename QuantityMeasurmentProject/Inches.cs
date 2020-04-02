903e2d351764ab5928c5b0f2cd679ce435c5f204
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-03 00:24:36 +0530

    [sushant]Add .Added the test case similler to Feet 1.7 to 1.12 fail test according to TDD approach

--- QuantityMeasurmentProject/Inches.cs
+++ QuantityMeasurmentProject/Inches.cs
@@ -6,5 +6,25 @@ namespace QuantityMeasurmentProject
 {
     public class Inches
     {
+        int inch;
+        public Inches(int feet)
+        {
+            this.inch = inch;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+                return false;
+            else
+                return true;
+
+        }
+        public bool ConvertInchValue(Feet inch)
+        {
+            if (this.inch.Equals(inch))
+                return true;
+            else
+                return false;
+        }
     }
 }
