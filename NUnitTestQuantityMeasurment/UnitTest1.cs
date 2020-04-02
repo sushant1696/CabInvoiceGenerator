fd9fccf18b11bf34fdaa628b3fe2f32829793502
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-02 22:17:46 +0530

    [sushant]Refactore .TestCase_1.2 null check fail test

--- NUnitTestQuantityMeasurment/UnitTest1.cs
+++ NUnitTestQuantityMeasurment/UnitTest1.cs
@@ -5,16 +5,28 @@ namespace NUnitTestQuantityMeasurment
 {
     public class Tests
     {
+       
         /// <summary>
         /// Test Case 1.1 where given 0 feet and return 0 feet equals
         /// </summary>
-      [Test]
+        [Test]
       public void GivenFeet_ShouldReturnFeetEqual()
         {
             Feet ft = new Feet(0);
             Feet ft2 = new Feet(0);
             var result = ft.ConvertFeetValue(ft2);
             Assert.IsTrue(result);
+        }
+        /// <summary>
+        /// test case 1.2 (null check)
+        /// </summary>
+        [Test]
+        public void GivenNull_ShouldReturFalse()
+        {
+            Feet ft1 = new Feet(11);
+            var actual = ft1.Equals(null);
+            Assert.IsFalse(actual);
+
         }
 
     }
