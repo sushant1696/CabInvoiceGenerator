6168cb27ef6ea0ad602f50a23cf1bb0892e450ed
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-02 21:19:39 +0530

    [sushant]Add .TestCase_1.1_of_UC_1_fail_test_according to TDD approach

--- NUnitTestQuantityMeasurment/UnitTest1.cs
+++ NUnitTestQuantityMeasurment/UnitTest1.cs
@@ -1,18 +1,21 @@
 using NUnit.Framework;
+using QuantityMeasurmentProject;
 
 namespace NUnitTestQuantityMeasurment
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        /// <summary>
+        /// Test Case 1.1 where given 0 feet and return 0 feet equals
+        /// </summary>
+      [Test]
+      public void GivenFeet_ShouldReturnFeetEqual()
         {
+            Feet ft = new Feet(0);
+            Feet ft2 = new Feet(0);
+            var result = ft.ConvertFeetValue(ft2);
+            Assert.IsTrue(result);
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
 }
---
+++ QuantityMeasurmentProject/Yard.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentProject
+{
+    public class Yard
+    {
+    }
+}
