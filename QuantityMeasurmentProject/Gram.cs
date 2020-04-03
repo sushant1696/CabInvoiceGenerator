325980fa75c3d8e9d2f723f5e0f4b4ed33f824fe
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-03 15:30:05 +0530

    [sushant]Add .Test case of Kilogram to gram compare test case

---
+++ QuantityMeasurmentProject/Gram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentProject
+{
+   public class Gram
+    {
+        public double grm;
+        public Gram(double grm)
+        {
+            this.grm = grm;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+                return false;
+            else
+                return true;
+
+        }
+        public bool ConvertFeetValue(Gram grm)
+        {
+            if (this.grm.Equals(grm.grm))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ QuantityMeasurmentProject/Kilogram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentProject
+{
+   public class Kilogram
+    {
+        public double klgm;
+        public Kilogram(double klgm)
+        {
+            this.klgm = klgm;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+                return false;
+            else
+                return true;
+
+        }
+        public bool ConvertFeetValue(Kilogram klgm)
+        {
+            if (this.klgm.Equals(klgm.klgm))
+                return true;
+            else
+                return false;
+        }
+    }
+}
