31bb127ce23a19f41c1bdfb320f8e217a43872a6
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-04-03 12:40:26 +0530

    [sushant]Refactore .Implement the Yard class with Busyness logic

--- QuantityMeasurmentProject/Yard.cs
+++ QuantityMeasurmentProject/Yard.cs
@@ -4,5 +4,27 @@ using System.Text;
 
 namespace QuantityMeasurmentProject
 {
-    
+    public class Yard
+    {
+        public double yrd;
+        public Yard(double yrd)
+        {
+            this.yrd = yrd;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+                return false;
+            else
+                return true;
+
+        }
+        public bool ConvertYardValue(Yard yrd)
+        {
+            if (this.yrd.Equals(yrd.yrd))
+                return true;
+            else
+                return false;
+        }
+    }
 }
